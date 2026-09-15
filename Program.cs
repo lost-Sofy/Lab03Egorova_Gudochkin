ced9216d88d7c157e0f663c15ea6095db5baa1d7
lost-sofy <sofiaegorova4452@gmail.com>
2026-09-15 14:53:13 +0300

    Lab03: шаг 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счёт");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500;
+Console.WriteLine($"после пополнения на 500: {balance}");
+
+balance -= 200;
+Console.WriteLine($"после покупки а 200: {balance}");
+
+balance *= 1.05;
+Console.WriteLine($"после начисления 5%: {balance}");
+
+balance /= 2;
+Console.WriteLine($"после деления пополам: {balance}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
