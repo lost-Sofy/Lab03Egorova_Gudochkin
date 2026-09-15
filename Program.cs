f62502e1c204df920f9608458d39527a5c1e365d
lost-sofy <sofiaegorova4452@gmail.com>
2026-09-15 15:11:05 +0300

    Lab03: шаг 10

--- Program.cs
+++ Program.cs
@@ -1,37 +1,39 @@
-﻿Console.WriteLine("Банковский счёт");
+﻿// Console.WriteLine("Банковский счёт");
 
-double balance = 1000;
-Console.WriteLine($"Начальный баланс: {balance}");
+// double balance = 1000;
+// Console.WriteLine($"Начальный баланс: {balance}");
 
-balance += 500;
-Console.WriteLine($"после пополнения на 500: {balance}");
+// balance += 500;
+// Console.WriteLine($"после пополнения на 500: {balance}");
 
-balance -= 200;
-Console.WriteLine($"после покупки а 200: {balance}");
+// balance -= 200;
+// Console.WriteLine($"после покупки а 200: {balance}");
 
-balance *= 1.05;
-Console.WriteLine($"после начисления 5%: {balance}");
+// balance *= 1.05;
+// Console.WriteLine($"после начисления 5%: {balance}");
 
-balance /= 2;
-Console.WriteLine($"после деления пополам: {balance}");
-
-
+// balance /= 2;
+// Console.WriteLine($"после деления пополам: {balance}");
 
 
+Console.WriteLine();
+Console.WriteLine("Постфикс vs префикс");
 
+int cc = 1;
+Console.WriteLine($"cc++ выводит: {cc++}");
+Console.WriteLine($"После этого cc = {cc}");
 
+int ss = 1;
+Console.WriteLine($"++ss = {++ss}");
+Console.WriteLine($"После этого ss = {ss}");
 
+Console.WriteLine();
+Console.WriteLine("ловушка");
 
-
-
-
-
-
-
-
-
-
-
+int kk = 0;
+Console.WriteLine($"Попытка №{++kk}");
+Console.WriteLine($"Попытка №{++kk}");
+Console.WriteLine($"всего Попыток №{kk}");
 
 
 
